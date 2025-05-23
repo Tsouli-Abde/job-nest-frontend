db465062f2f9c908b0233ded949112151af240d2
lizyov <liza.khellaf.etudes@gmail.com>
2025-05-23 12:49:15 +0200

    Add a filter on job offers

--- src/app/pages/job-list/job-list.component.ts
+++ src/app/pages/job-list/job-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { JobService } from '../../services/job.service';
 import { Job } from '../../models/job';
 
@@ -9,11 +10,45 @@ import { Job } from '../../models/job';
 })
 export class JobListComponent implements OnInit {
   jobs: Job[] = [];
+  filterForm!: FormGroup;
 
-  constructor(private jobService: JobService) {}
+  constructor(private fb: FormBuilder, private jobService: JobService) {}
 
   ngOnInit(): void {
-    this.jobService.getJobs().subscribe((data) => {
+    this.filterForm = this.fb.group({
+      location: [''],
+      type: [''],
+      experienceLevel: [''],
+      salaryMin: [''],
+      salaryMax: ['']
+    });
+
+    this.fetchJobs();
+  }
+
+  applyFilters(): void {
+    const rawFilters = this.filterForm.value;
+  
+    const filters: any = {};
+    Object.keys(rawFilters).forEach(key => {
+      const value = rawFilters[key];
+      if (value !== null && value !== '') {
+        if (key === 'salaryMin' || key === 'salaryMax') {
+          filters[key] = Number(value);
+        } else {
+          filters[key] = value;
+        }
+      }
+    });
+  
+    this.jobService.getFilteredJobs(filters).subscribe({
+      next: (jobs) => this.jobs = jobs,
+      error: (err) => console.error(err)
+    });
+  }   
+
+  fetchJobs(): void {
+    this.jobService.getAllJobsSorted().subscribe((data) => {
       this.jobs = data;
     });
   }
--- src/app/services/job.service.ts
+++ src/app/services/job.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Job } from '../models/job';
 
@@ -9,7 +9,7 @@ import { Job } from '../models/job';
 export class JobService {
   private apiUrl = 'http://localhost:8080/api/v1/jobs';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getJobs(): Observable<Job[]> {
     return this.http.get<Job[]>(this.apiUrl);
@@ -21,5 +21,31 @@ export class JobService {
 
   getJobById(id: string): Observable<Job> {
     return this.http.get<Job>(`${this.apiUrl}/${id}`);
-  }  
+  }
+
+  getFilteredJobs(filters: any): Observable<Job[]> {
+    let params = new HttpParams();
+
+    if (filters.location) {
+      params = params.set('location', filters.location);
+    }
+    if (filters.type) {
+      params = params.set('type', filters.type);
+    }
+    if (filters.experienceLevel) {
+      params = params.set('experienceLevel', filters.experienceLevel);
+    }
+    if (filters.salaryMin) {
+      params = params.set('salaryMin', filters.salaryMin);
+    }
+    if (filters.salaryMax) {
+      params = params.set('salaryMax', filters.salaryMax);
+    }
+
+    return this.http.get<Job[]>(`${this.apiUrl}/filter`, { params });
+  }
+
+  getAllJobsSorted(): Observable<Job[]> {
+    return this.http.get<Job[]>(`${this.apiUrl}`);
+  }
 }
