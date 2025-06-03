148f6f1438c5710246e6ae3d1f5dfe80677fdc4c
lizyov <liza.khellaf.etudes@gmail.com>
2025-06-03 21:52:50 +0200

    Add company name to "My applications" & simplify application model

--- src/app/models/application.model.ts
+++ src/app/models/application.model.ts
@@ -1,29 +1,12 @@
-export interface Application {
-    coverLetter: string;
-    applicationDate: Date;
+import { Applicant } from "./applicant.model";
+import { Job } from "./job";
 
-    // Relations
-    applicantId: string;
-    jobId: string;
+export interface Application {
+  coverLetter: string;
+  applicationDate: Date;
 
   status: 'PENDING' | 'ACCEPTED' | 'REJECTED' | 'INTERVIEW';
 
-  applicant: {
-      id: string;
-      firstName: string;
-      lastName: string;
-      email: string;
-      phoneNumber: string;
-    };
-
-    job: {
-      id: string;
-      title: string;
-      location: string;
-      salaryMin: number;
-      salaryMax: number;
-      type: string;
-      experienceLevel: string;
-      postedAt: Date;
-    };
-  }
+  applicant: Applicant;
+  job: Job
+}
